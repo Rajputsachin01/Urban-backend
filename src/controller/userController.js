85c9a9d791e641368c70f267ad2b7f373439f9b0
Rajputsachin01 <rajputsachin8878@gmail.com>
2025-04-07 11:35:46 +0530

    final updations

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,9 +1,8 @@
-const UserModel = require("../models/userModel")
-const { signInToken } = require("../utils/auth")
-const Helper = require("../utils/helper")
-const bcrypt = require('bcryptjs')
+const UserModel = require("../models/userModel");
+const { signInToken } = require("../utils/auth");
+const Helper = require("../utils/helper");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
-
 async function getUserWithToken(userId) {
   try {
     let userDetail = await userProfile(userId);
@@ -28,21 +27,14 @@ const userProfile = async (userId) => {
     return false;
   }
 };
-   //for generating 6 digit random otp
-   const generateOTP = () =>
-    Math.floor(100000 + Math.random() * 900000).toString();
-   //For creating user
-const register = async (req, res) => {
+//for generating 6 digit random otp
+const generateOTP = () =>
+  Math.floor(100000 + Math.random() * 900000).toString();
+//For creating user
+const registerUser = async (req, res) => {
   try {
-
-    const { 
-      email,
-      password,
-      phoneNo,
-      address,
-      location,
-      referralCode,
-    } = req.body;
+    const { email, password, phoneNo, address, location, referralCode } =
+      req.body;
 
     // validation for required field
     if (!email) {
@@ -65,24 +57,24 @@ const register = async (req, res) => {
     if (!emailRegex.test(email)) {
       return Helper.fail(res, "Email is not valid!");
     }
-       // Validating phone no.
-       if (phoneNo) {
-        const phoneRegex = /^\d{6,14}$/; // Allows only digits(0-9) and ensures length is between 6 and 14
-  
-        if (!phoneRegex.test(phoneNo)) {
-          return Helper.fail(res, " number is not valid!");
-        }
+    // Validating phone no.
+    if (phoneNo) {
+      const phoneRegex = /^\d{6,14}$/; // Allows only digits(0-9) and ensures length is between 6 and 14
+      if (!phoneRegex.test(phoneNo)) {
+        return Helper.fail(res, " number is not valid!");
       }
-
+    }
     let checkObj = { $or: [{ email: email }] };
     if (phoneNo) {
       checkObj.$or.push({ phoneNo: phoneNo });
     }
     let userCheck = await UserModel.find(checkObj);
     if (userCheck.length > 0) {
-      return Helper.fail(res, "User already exists with this email or mobile No.!");
+      return Helper.fail(
+        res,
+        "User already exists with this email or mobile No.!"
+      );
     }
- 
     // Generate unique referral code
     const generateReferralCode = async () => {
       let isUnique = false,
@@ -114,46 +106,147 @@ const register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     // Generate OTP
     const otp = generateOTP();
-    const userObj = { 
-      email, 
-      password, 
-      phoneNo, 
-      address, 
+    const userObj = {
+      email,
+      password,
+      phoneNo,
+      address,
       location,
       password: hashedPassword,
       otp: otp,
       referralCode: newReferralCode,
       referredBy,
-     };
+    };
 
     const createUser = await UserModel.create(userObj);
-     return Helper.success(res, "User registered successfully", createUser);
+    return Helper.success(res, "User registered successfully", createUser);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Internal server error" })
-  } ////
-// Generate JWT token and user details
-const { token, userDetail } = await getUserWithToken(user._id);
-if (!token || !userDetail) {
-  return Helper.error(res, "Failed to generate token or get user profile");
-}
-if (user.referredBy) {
-  const referralBonus = 100;
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+//for updating User
+const updateUser = async (req, res) => {
+  try {
+    let userId = req.userId;
+    const { email, phoneNo, password,address,location, } =
+      req.body;
+    if (!userId) {
+      return Helper.fail(res, "userId is missing from request");
+    }
+    //validating email
+    if (email) {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if (!emailRegex.test(email)) {
+        return Helper.fail(res, "Email is not valid!");
+      }
+    }
+    // Validate mobile if provided
+    if (phoneNo) {
+      const phoneRegex = /^\d{6,14}$/;
+      if (!phoneRegex.test(phoneNo)) {
+        return Helper.fail(res, "phoneNo is not valid!");
+      }
+    }
+    let user = await UserModel.findById(userId);
+    if (!user) {
+      return Helper.fail(res, "user not found!");
+    }
+    let objToUpdate = {};
+    if (phoneNo) {
+      objToUpdate.phoneNo = phoneNo;
+    }
+    if (address) {
+      objToUpdate.address = address;
+    }
+    if (location) {
+      objToUpdate.location = location;
+    }
+    if (email) {
+      const emailRegex = new RegExp(`^${email}$`, "i");
+      const user = await UserModel.findOne({
+        email: emailRegex,
+        _id: { $ne: userId },
+      });
 
-  await WalletModel.findOneAndUpdate(
-    { userId: user.referredBy },
-    { $inc: { points: referralBonus } },
-    { upsert: true } // Creates wallet if not exists
-  );
-
-  console.log(`100 points credited to referrer: ${user.referredBy}`);
-}
-
-return Helper.success(res, "Token generated successfully.", {
-  token,
-  userDetail, 
-}); ///
-}
+      if (user) {
+        return Helper.fail(res, "Email is already used in another account");
+      }
+      objToUpdate.email = email;
+    }
+    let updatedProfile = await UserModel.findByIdAndUpdate(
+      userId,
+      objToUpdate,
+      {
+        new: true,
+      }
+    );
+    if (updatedProfile) {
+      return Helper.success(res, "User  updated successfully!", updatedProfile);
+    }
+  } catch (error) {
+    console.log(error);
+    return Helper.fail(res, error.message);
+  }
+};
+//for soft delete User
+const removeUser = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    if (!userId) {
+      return Helper.fail(res, "Please provide User Id ");
+    }
+    let i = { _id: userId };
+    let deleted = await UserModel.findOneAndUpdate(
+      i,
+      { isDeleted: true },
+      { new: true }
+    );
+    if (!deleted) {
+      return Helper.fail(res, "No user found!");
+    }
+    return Helper.success(res, " User deleted successfully", deleted);
+  } catch (error) {
+    console.log(error);
+    return Helper.fail(res, error.message);
+  }
+};
+//for fetching user profile
+const fetchProfile = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await UserModel.findById(userId).select({
+      password: 0,
+      __v: 0,
+      createdAt: 0,
+      updatedAt: 0,
+    });
+    if (!user) {
+      return Helper.fail(res, "user not found");
+    }
+    return Helper.success(res, "Profile fetched successfully",user);
+  } catch (error) {
+    console.log(error);
+    return Helper.fail(res, "Failed to fetch profile");
+  }
+};
+//for finding user By UserId
+const findUserById = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    if (!userId) {
+      return Helper.fail(res, "userId is required");
+    }
+    const user = await UserModel.findOne({ _id: userId, isDeleted: false });
+    if (!user) {
+      return Helper.fail(res, "User not found");
+    }
+    return Helper.success(res, "User found", user);
+  } catch (error) {
+    console.log(error);
+    return Helper.fail(res, "Failed to fetch user");
+  }
+};
 //for verifying OTP
 const verifyOTP = async (req, res) => {
   try {
@@ -215,11 +308,36 @@ const verifyOTP = async (req, res) => {
     return Helper.fail(res, error.message);
   }
 };
-//for user login
-const login = async (req, res) => {
+const resendOTP = async (req, res) => {
   try {
-    const { phoneNo, email, password} = req.body;
+    const { email, number } = req.body;
 
+    if (!email && !number) {
+      return Helper.fail(res, "Please provide an email or phone number.");
+    }
+    // Find user using email or number
+    const user = await UserModel.findOne({
+      $or: [{ number }, { email }],
+    });
+
+    if (!user) {
+      return Helper.fail(res, "User not found!");
+    }
+    // Generate new OTP and set expiry
+    // generateOTP();
+    const otp = 123456;
+    const otpExpires = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
+    await UserModel.updateOne({ _id: user._id }, { $set: { otp, otpExpires } });
+    return Helper.success(res, "OTP resent successfully.");
+  } catch (error) {
+    console.error(error);
+    return Helper.fail(res, error.message);
+  }
+};
+//for user login
+const loginUser = async (req, res) => {
+  try {
+    const { phoneNo, email, password } = req.body;
     if (!password) {
       return Helper.fail(res, "Please enter password");
     }
@@ -235,27 +353,27 @@ const login = async (req, res) => {
         Boolean
       ),
     });
-    const otp = generateOTP();
-    // console.log(otp);
-
+    // const otp = generateOTP();
     if (!user) {
-      return Helper.fail(res, "Email not found, please enter a valid email!");
+      return Helper.fail(res, "User not found ");
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return Helper.fail(res, "Invalid password");
     }
-
-    return Helper.success(res, "Login successful", { user });
-
+    return Helper.success(res, "Login successfull", user);
   } catch (error) {
     console.log(error);
     return Helper.fail(res, "Login failed");
   }
 };
 module.exports = {
-  register,
-  login,
-  verifyOTP
-  
+  registerUser,
+  updateUser,
+  removeUser,
+  fetchProfile,
+  findUserById,
+  loginUser,
+  verifyOTP,
+  resendOTP,
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,66 +1,62 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        default: "",
-        unique: true,
-        lowercase: true,
+      type: String,
+      required: true,
+      default: "",
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: true,
-        default: "",
+      type: String,
+      required: true,
+      default: "",
     },
     phoneNo: {
-        type: Number,
-        required: true,
-        default: "",
+      type: Number,
+      required: true,
+      default: "",
     },
     address: {
-        type: String,
-        required: true,
-        default: "",
+      type: String,
+      required: true,
+      default: "",
     },
     location: {
-        type: String,
-        required: true,
-        default: "",
+      type: { lat: Number, lng: Number },
+      required: true,
     },
     referralCode: {
-        type: String,
-        default:""
+      type: String,
+      default: "",
     },
-    referredBy:{
-        type: mongoose.Schema.Types.ObjectId, ref: "users",
-        default: ""
+    referredBy: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+    otp: {
+      type: String,
     },
-    otp: { 
-        type: String 
-    },
-}, 
- { timestamps: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
 // For passswrod Hash
 UserSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        console.log("Password not modified, skipping hashing.");
-        return next();
-    }
+  if (!this.isModified("password")) {
+    console.log("Password not modified, skipping hashing.");
+    return next();
+  }
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        console.log("Password hashed successfully.");
-        next();
-    } catch (error) {
-        console.error("Error hashing password:", error);
-        next(error);
-    }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log("Password hashed successfully.");
+    next();
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    next(error);
+  }
 });
-
-
 
 module.exports = mongoose.model("users", UserSchema);
