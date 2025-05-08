4d1d88eda3f5066339b2b9591f7ad6b9fba9440c
Ravina Birla <ravinabilra69@gmail.com>
2025-05-08 15:16:22 +0530

    update

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -23,11 +23,10 @@ const {
 router.post("/initiateBooking", isAuth, initiateBooking);
 router.post("/getDateAndTimeslot", isAuth, getDateAndTimeslot);
 router.post("/location", isAuth, getLocationAndAddress);
-// router.post("/addbooking",isAuth, addBooking)
 router.post("/update/:id", isAuth, updateBooking);
 router.post("/delete", isAuth, removeBooking);
 router.post("/userbookings", isAuth, fetchUserBooking);
-router.post("/historyOrPanding", isAuth, userBookingHistoryOrPending);
+router.post("/historyOrPending", isAuth, userBookingHistoryOrPending);
 router.post("/cancel/:id", isAuth, cancelBooking);
 router.post("/findById/:id", isAuth, findBookingById);
 router.post("/usersBookingListing", isAuth, usersBookingListing);
